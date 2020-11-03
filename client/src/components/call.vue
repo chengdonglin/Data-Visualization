24423263a721c4cca194d618ab67de5f1bd2615f
linchengdong <linchengdong@ins24.com>
2020-11-03 17:27:26 +0800

    feat: 屏幕自适配

--- client/src/components/call.vue
+++ client/src/components/call.vue
@@ -2,7 +2,7 @@
  * @Description: 外呼数据统计横向柱状图
  * @Author: chengDong
  * @Date: 2020-10-28 14:06:45
- * @LastEditTime: 2020-11-03 16:32:51
+ * @LastEditTime: 2020-11-03 17:23:52
  * @LastEditors: chengDong
 -->
 <template>
@@ -25,25 +25,10 @@ export default {
         // 初始化echartsInstance对象
         initChart() {
             this.chartInstance = this.$echarts.init(this.$refs.call_ref,'dark')
-        },
-        // 获取服务器数据
-        async getData() {
-            const { data } = await this.$api.call.callData()
-            this.allData =  data
-            this.updateChart()
-        },
-        // 更新图表
-        updateChart() {
-            const callName = this.allData.map(item => {
-                return item.name
-            })
-            const callValues = this.allData.map(item => {
-                return item.value
-            })
-            const option = {
-                // 标题配置
+            const initOption = {
+                     // 标题配置
                 title:{
-                    text:'丨外呼相关量统计',
+                    text:'丨外呼相关量统计(单位: 次)',
                     textStyle:{
                         fontSize: 60
                     },
@@ -59,11 +44,12 @@ export default {
                     containLabel: true // 举例包含坐标轴上文字
                 },
                 xAxis:{
-                    type: 'value'
+                    type: 'value',
+                    name:'次数'
                 },
                 yAxis:{
                     type: 'category',
-                    data: callName
+                    name: '维度'
                 },
                 // 鼠标移到x坐标轴背景变化
                 tooltip:{
@@ -80,7 +66,6 @@ export default {
                 series:[
                     {
                         type: 'bar',
-                        data: callValues,
                         barWidth: 66, // 柱的宽度,
                         // 文字控制
                         label:{
@@ -108,12 +93,76 @@ export default {
                     }
                 ]
             }
-            this.chartInstance.setOption(option)
+            this.chartInstance.setOption(initOption)
+        },
+        // 获取服务器数据
+        async getData() {
+            const { data } = await this.$api.call.callData()
+            this.allData =  data
+            this.updateChart()
+        },
+        // 更新图表
+        updateChart() {
+            const callName = this.allData.map(item => {
+                return item.name
+            })
+            const callValues = this.allData.map(item => {
+                return item.value
+            })
+            const dataOption = {
+                yAxis:{
+                    data: callName,
+                },
+                series:[
+                    {
+                        data: callValues
+                    }
+                ]
+            }
+            this.chartInstance.setOption(dataOption)
+        },
+        // 浏览器大小发生变化的时候,调用的方法,来完成屏幕的适配
+        screenAdapter() {
+           const titleFontSize = this.$refs.call_ref.offsetWidth / 100 * 3.6
+           // 分辨率相关配置项
+           const adapterOption = {
+                   title:{
+                    textStyle:{
+                        fontSize: titleFontSize
+                    }
+                },
+                // 鼠标移到x坐标轴背景变化
+                tooltip:{
+                    axisPointer:{
+                        lineStyle:{
+                            width: titleFontSize,
+                        }
+                    }
+                },
+                series:[
+                    {
+                        barWidth: titleFontSize, // 柱的宽度,
+                        // 柱左上角,右上角, 左下角,右下角的圆角设置 
+                        itemStyle:{
+                            barBorderRadius: [0,titleFontSize / 2 ,titleFontSize / 2,0],
+                        }
+                    }
+                ]
+           }
+           this.chartInstance.setOption(adapterOption)
+           // 手动调动对象 resize
+           this.chartInstance.resize()
         }
     },
     mounted () {
         this.initChart()
         this.getData()
+        window.addEventListener('resize',this.screenAdapter)
+        // 页面加载完成, 主动进行屏幕适配
+        this.screenAdapter()
+    },
+    destroyed () {
+        window.removeEventListener('resize', this.screenAdapter)
     }
 }
 </script>
