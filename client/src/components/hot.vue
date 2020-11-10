992fc5d40e28c526c990129061d1afef082b5367
linchengdong <linchengdong@ins24.com>
2020-11-10 15:40:21 +0800

    feat: 切换数据

--- client/src/components/hot.vue
+++ client/src/components/hot.vue
@@ -2,14 +2,14 @@
  * @Description: 
  * @Author: chengDong
  * @Date: 2020-11-10 14:07:48
- * @LastEditTime: 2020-11-10 15:21:25
+ * @LastEditTime: 2020-11-10 15:39:33
  * @LastEditors: chengDong
 -->
 <template>
   <div class="com-container">
-      <div class="com-charts" ref="hot_ref">
-          
-      </div>
+      <div class="com-charts" ref="hot_ref"></div>
+        <span class="iconfont arr_left" @click="toLeft">next</span>
+        <span class="iconfont arr_right" @click="toRight">up</span>
   </div>
 </template>
 
@@ -36,7 +36,7 @@ export default {
             this.chartInstance = this.$echarts.init(this.$refs.hot_ref,'dark')
             const initOption = {
                 title: {
-                    text: '! 热销商品销售金额占比统计',
+                    text: '| 热销商品销售金额占比统计',
                     left: 20,
                     top: 20
                 },
@@ -82,11 +82,40 @@ export default {
             const adapterOption = {}
             this.chartInstance.setOption(adapterOption)
             this.chartInstance.resize()
+        },
+        toLeft() {
+            this.currentIndex--
+            if(this.currentIndex < 0) {
+                this.currentIndex = this.allData.length -1
+            }
+            this.updateChart()
+        },
+        toRight() {
+            this.currentIndex++
+            if(this.currentIndex > this.allData.length -1) {
+                this.currentIndex = 0
+            }
+            this.updateChart()
         }
     }
 }
 </script>
 
 <style lang="less" scoped>
-
+.arr_left {
+    position: absolute;
+    left: 10%;
+    top: 50%;
+    transform: translateY(-80%);
+    color: white;
+  
+}
+.arr_right {
+    position: absolute;
+    right: 10%;
+    top: 50%;
+    transform: translateY(-80%);
+    cursor: pointer;
+    color: white
+}
 </style>
